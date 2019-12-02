06830309db106bd6eccd5806d7f20d355b76179a
George Minaya Asencios <gminaya6524@gmail.com>
2019-12-02 03:30:30 -0500

    Corregido problema con la bd

--- ExamenNet/ExamenTecnico.Net/ExamenTecnico.Net.Parte2.DA/ConexionSQL.cs
+++ ExamenNet/ExamenTecnico.Net/ExamenTecnico.Net.Parte2.DA/ConexionSQL.cs
@@ -4,6 +4,7 @@ namespace ExamenTecnico.Net.Parte2.DA
 {
     public class ConexionSQL
     {
-        public static string DB_Bancos() => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BD_Bancos;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+         public static string DB_Bancos() => @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog="+ 
+                  System.IO.Directory.GetCurrentDirectory() + @"\BD_BANCOS.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
 }
